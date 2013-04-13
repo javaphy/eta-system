adbb541971892d56292fd94b8eff1e0253bbf9ab
Chi Chan <chihinchan@gmail.com>
2013-04-13 19:42:17 -0400

    Added update form.

--- ETA_system/Controllers/HomeController.cs
+++ ETA_system/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using ETA_system.Models;
 using ETA_system.Repositories;
@@ -24,6 +25,13 @@ namespace ETA_system.Controllers
         [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.Rows = repository.GetAll();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
             ViewBag.Provinces = provinces;
             return View();
         }
@@ -31,12 +39,66 @@ namespace ETA_system.Controllers
         [HttpPost]
         public ActionResult Add(WorkOrder input)
         {
+            input.Status = WorkOrder.StatusTypes.Picking;
             input.ReceivedInPreflightTime = DateTime.Now;
-            input.EstimatedTimeofComplete = DateTime.Now.AddDays(5);
+            input.EstimatedTimeOfComplete = DateTime.Now.AddDays(5);
+
+            input.OrderNumber = input.OrderNumber.Trim().ToUpper();
 
             repository.SaveOrUpdate(input);
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Management()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public ActionResult PendingComplete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PendingComplete(PendingCompleteInput input)
+        {
+            if(!Update(input))
+                Redirect("OrderNumberNotFound");
+
+            return RedirectToAction("Index");
+        }
+
+        private bool Update(PendingCompleteInput input)
+        {
+            input.OrderNumber = input.OrderNumber.Trim().ToUpper();
+
+            var workOrder = repository.Get(x => x.OrderNumber == input.OrderNumber).FirstOrDefault();
+
+            if (workOrder == null)
+                return false;
+
+            var status = WorkOrder.StatusTypes.Complete;
+
+            if (input.Submit == "Pending")
+                status = WorkOrder.StatusTypes.Pending;
+            
+            
+            workOrder.Status = status;
+            workOrder.Comments = input.Comments;
+            workOrder.ActualPreflightCompleteTime = DateTime.Now;
+
+            repository.SaveOrUpdate(workOrder);
+
+            return true;
+        }
+
+        public class PendingCompleteInput
+        {
+            public string OrderNumber { get; set; }
+            public string Comments { get; set; }
+            public string Submit { get; set; }
         }
     }
 }
--- ETA_system/Models/WorkOrder.cs
+++ ETA_system/Models/WorkOrder.cs
@@ -12,15 +12,25 @@ namespace ETA_system.Models
         public virtual string CustomerName { get; set; }
         public virtual string MachineModel { get; set; }
         public virtual string MachineSerialNumber { get; set; }
-        public virtual string Status { get; set; }
+        public virtual StatusTypes Status { get; set; }
         public virtual string Province { get; set; }
         public virtual DateTime PlaceOrderTime { get; set; }
         public virtual DateTime PrintOrderTime { get; set; }
         public virtual DateTime ReceivedInPreflightTime { get; set; }
-        public virtual DateTime EstimatedTimeofComplete { get; set; }
+        public virtual DateTime EstimatedTimeOfComplete { get; set; }
         public virtual DateTime PlannedDeliveryTime { get; set; }
         public virtual DateTime? ActualPreflightCompleteTime { get; set; }
         public virtual string TechName { get; set; }
         public virtual string Comments { get; set; }
+
+
+        public enum StatusTypes
+        {
+            Picking = 1,
+            Waiting = 2,
+            Processing = 3,
+            Pending = 4,
+            Complete = 5,
+        }
     }
 }
